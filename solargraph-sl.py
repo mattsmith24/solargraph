ed8598643654529f6d5de4e87be2d722fa26db9e
Matthew Smith <yo.checkit@gmail.com>
2022-10-23 20:52:35 +1000

    Move controls to sidebar

--- solargraph-sl.py
+++ solargraph-sl.py
@@ -14,7 +14,7 @@ SOLARLOGGING_DB_PATH = os.path.join(SOLARLOGGING_DATA_DIR, "solarlogging.db")
 sqlcon = sqlite3.connect(SOLARLOGGING_DB_PATH)
 sqlcon.row_factory = sqlite3.Row
 
-timescale = st.selectbox(
+timescale = st.sidebar.selectbox(
     'Timescale',
      [
         'Samples',
@@ -30,43 +30,43 @@ timescale = st.selectbox(
 cur = sqlcon.cursor()
 
 if timescale == 'Samples':
-    hours_ago = st.slider('Hours ago', min_value=1, max_value=48, value=24)
-    hours_to_show = st.slider('Hours to show', min_value=1, max_value=48, value=24)
+    hours_ago = st.sidebar.slider('Hours ago', min_value=1, max_value=48, value=24)
+    hours_to_show = st.sidebar.slider('Hours to show', min_value=1, max_value=48, value=24)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=hours_ago)
     end_time = start_time + datetime.timedelta(hours=hours_to_show)
     res = cur.execute("SELECT * from samples where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == '5 Minute':
-    hours_ago = st.slider('Hours ago', min_value=1, max_value=250, value=24)
-    hours_to_show = st.slider('Hours to show', min_value=1, max_value=250, value=24)
+    hours_ago = st.sidebar.slider('Hours ago', min_value=1, max_value=250, value=24)
+    hours_to_show = st.sidebar.slider('Hours to show', min_value=1, max_value=250, value=24)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=hours_ago)
     end_time = start_time + datetime.timedelta(hours=hours_to_show)
     res = cur.execute(f"SELECT * from fiveminute where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Hourly':
-    days_ago = st.slider('Days ago', min_value=1, max_value=14, value=7)
-    days_to_show = st.slider('Days to show', min_value=1, max_value=14, value=7)
+    days_ago = st.sidebar.slider('Days ago', min_value=1, max_value=14, value=7)
+    days_to_show = st.sidebar.slider('Days to show', min_value=1, max_value=14, value=7)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=days_ago)
     end_time = start_time + datetime.timedelta(days=days_to_show)
     res = cur.execute(f"SELECT * from hourly where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Daily':
-    weeks_ago = st.slider('Weeks ago', min_value=1, max_value=8, value=4)
-    weeks_to_show = st.slider('Weeks to show', min_value=1, max_value=8, value=4)
+    weeks_ago = st.sidebar.slider('Weeks ago', min_value=1, max_value=8, value=4)
+    weeks_to_show = st.sidebar.slider('Weeks to show', min_value=1, max_value=8, value=4)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(weeks=weeks_ago)
     end_time = start_time + datetime.timedelta(weeks=weeks_to_show)
     res = cur.execute(f"SELECT * from daily where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Weekly':
-    months_ago = st.slider('Months ago', min_value=1, max_value=24, value=12)
-    months_to_show = st.slider('Months to show', min_value=1, max_value=24, value=12)
+    months_ago = st.sidebar.slider('Months ago', min_value=1, max_value=24, value=12)
+    months_to_show = st.sidebar.slider('Months to show', min_value=1, max_value=24, value=12)
     start_time = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=months_ago*31)).replace(day=1)
     end_time = (start_time + datetime.timedelta(days=months_to_show*31)).replace(day=1)
     res = cur.execute(f"SELECT * from weekly where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Monthly':
-    years_ago = st.slider('Years ago', min_value=1, max_value=10, value=2)
-    years_to_show = st.slider('Years to show', min_value=1, max_value=10, value=2)
+    years_ago = st.sidebar.slider('Years ago', min_value=1, max_value=10, value=2)
+    years_to_show = st.sidebar.slider('Years to show', min_value=1, max_value=10, value=2)
     start_time = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=366 * years_ago)).replace(day=1)
     end_time = (start_time + datetime.timedelta(days=years_to_show*366)).replace(day=1)
     res = cur.execute(f"SELECT * from monthly where timestamp > ? and timestamp < ? order by id",
@@ -84,11 +84,11 @@ for row in res:
     
 
 dataframe_dict = {}
-if st.checkbox('Show Grid', value=True):
+if st.sidebar.checkbox('Show Grid', value=True):
     dataframe_dict['Grid'] = np.array(grid_vals)
-if st.checkbox('Show Solar', value=True):
+if st.sidebar.checkbox('Show Solar', value=True):
     dataframe_dict['Solar'] = np.array(solar_vals)
-if st.checkbox('Show Home', value=True):
+if st.sidebar.checkbox('Show Home', value=True):
     dataframe_dict['Home'] = np.array(home_vals)
 
 dataframe = pd.DataFrame(data=dataframe_dict, index=np.array(timestamps))
