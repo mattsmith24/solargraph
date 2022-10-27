419a3eea91cf21fdf2f6aed4b8a1704f06187c7c
Matthew Smith <yo.checkit@gmail.com>
2022-10-27 13:18:06 +1000

    Change defaults towards smaller values

--- solargraph-sl.py
+++ solargraph-sl.py
@@ -25,49 +25,49 @@ timescale = st.sidebar.selectbox(
         'Weekly',
         'Monthly'
      ],
-     index=0
+     index=1
 )
 
 cur = sqlcon.cursor()
 
 if timescale == 'Samples':
     minutes_ago = st.sidebar.slider('Minutes ago', min_value=1, max_value=360, value=60)
-    minutes_to_show = st.sidebar.slider('Minutes to show', min_value=1, max_value=360, value=60)
+    minutes_to_show = st.sidebar.slider('Minutes to show', min_value=1, max_value=360, value=360)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=minutes_ago)
     end_time = start_time + datetime.timedelta(minutes=minutes_to_show)
     res = cur.execute("SELECT * from samples where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == '5 Minute':
-    hours_ago = st.sidebar.slider('Hours ago', min_value=1, max_value=250, value=24)
-    hours_to_show = st.sidebar.slider('Hours to show', min_value=1, max_value=250, value=24)
+    hours_ago = st.sidebar.slider('Hours ago', min_value=1, max_value=250, value=8)
+    hours_to_show = st.sidebar.slider('Hours to show', min_value=1, max_value=250, value=250)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=hours_ago)
     end_time = start_time + datetime.timedelta(hours=hours_to_show)
     res = cur.execute(f"SELECT * from fiveminute where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Hourly':
-    days_ago = st.sidebar.slider('Days ago', min_value=1, max_value=14, value=7)
-    days_to_show = st.sidebar.slider('Days to show', min_value=1, max_value=14, value=7)
+    days_ago = st.sidebar.slider('Days ago', min_value=1, max_value=14, value=2)
+    days_to_show = st.sidebar.slider('Days to show', min_value=1, max_value=14, value=14)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=days_ago)
     end_time = start_time + datetime.timedelta(days=days_to_show)
     res = cur.execute(f"SELECT * from hourly where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Daily':
-    weeks_ago = st.sidebar.slider('Weeks ago', min_value=1, max_value=8, value=4)
-    weeks_to_show = st.sidebar.slider('Weeks to show', min_value=1, max_value=8, value=4)
+    weeks_ago = st.sidebar.slider('Weeks ago', min_value=1, max_value=8, value=1)
+    weeks_to_show = st.sidebar.slider('Weeks to show', min_value=1, max_value=8, value=8)
     start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(weeks=weeks_ago)
     end_time = start_time + datetime.timedelta(weeks=weeks_to_show)
     res = cur.execute(f"SELECT * from daily where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Weekly':
-    months_ago = st.sidebar.slider('Months ago', min_value=1, max_value=24, value=12)
-    months_to_show = st.sidebar.slider('Months to show', min_value=1, max_value=24, value=12)
+    months_ago = st.sidebar.slider('Months ago', min_value=1, max_value=24, value=1)
+    months_to_show = st.sidebar.slider('Months to show', min_value=1, max_value=24, value=24)
     start_time = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=months_ago*31)).replace(day=1)
     end_time = (start_time + datetime.timedelta(days=(months_to_show+1)*31)).replace(day=1)
     res = cur.execute(f"SELECT * from weekly where timestamp > ? and timestamp < ? order by id",
         (start_time.isoformat(), end_time.isoformat()))
 elif timescale == 'Monthly':
-    years_ago = st.sidebar.slider('Years ago', min_value=1, max_value=10, value=2)
-    years_to_show = st.sidebar.slider('Years to show', min_value=1, max_value=10, value=2)
+    years_ago = st.sidebar.slider('Years ago', min_value=1, max_value=10, value=1)
+    years_to_show = st.sidebar.slider('Years to show', min_value=1, max_value=10, value=10)
     start_time = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=366 * years_ago)).replace(day=1)
     end_time = (start_time + datetime.timedelta(days=(years_to_show+1)*366)).replace(month=1,day=1)
     res = cur.execute(f"SELECT * from monthly where timestamp > ? and timestamp < ? order by id",
