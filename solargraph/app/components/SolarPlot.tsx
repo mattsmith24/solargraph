d92296a6af4cd80d0384e8964a412517339df88a
Matthew Smith <yo.checkit@gmail.com>
2025-12-18 12:09:51 +1000

    Add grid and home traces to the plot

--- solargraph/app/components/SolarPlot.tsx
+++ solargraph/app/components/SolarPlot.tsx
@@ -14,20 +14,34 @@ interface SolarStatus {
 }
 
 export default function SolarPlot({ samples }: { samples: SolarStatus[] }) {
-  const x = samples.map((sample: SolarStatus) => new Date(sample.timestamp))
-  const y = samples.map((sample: SolarStatus) => sample.solar)
+  const solar_trace = {
+    x: samples.map((sample: SolarStatus) => new Date(sample.timestamp)),
+    y: samples.map((sample: SolarStatus) => sample.solar),
+    type: 'scatter',
+    mode: 'lines',
+    marker: { color: 'green' },
+    name: 'Solar'
+  }
+  const grid_trace = {
+    x: samples.map((sample: SolarStatus) => new Date(sample.timestamp)),
+    y: samples.map((sample: SolarStatus) => sample.grid),
+    type: 'scatter',
+    mode: 'lines',
+    marker: { color: 'blue' },
+    name: 'Grid'
+  }
+  const home_trace = {
+    x: samples.map((sample: SolarStatus) => new Date(sample.timestamp)),
+    y: samples.map((sample: SolarStatus) => sample.home),
+    type: 'scatter',
+    mode: 'lines',
+    marker: { color: 'orange' },
+    name: 'Home'
+  }
 
   return (
     <Plot
-      data={[
-        {
-          x,
-          y,
-          type: 'scatter',
-          mode: 'lines',
-          marker: { color: 'red' },
-        },
-      ]}
+      data={[solar_trace, grid_trace, home_trace]}
       layout={{ width: 1200, height: 480, title: { text: 'Samples' } }}
     />
   );
